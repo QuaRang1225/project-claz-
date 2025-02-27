f7555b585d7623568cb9a567cf70548d367acf6e
QuaRang <dbduddnd1225@gmail.com>
2025-02-27 21:16:49 +0900

    #2 - 외부에서 공유하기 버튼 터치 시 나오는 뷰 구현
    - 해당 뷰는 클라제앱으로 공유하기 버튼을 눌렀을 때 나오는 SharedView를 구현한 ViewController임
    - 그 때 각 앱마다 받을 수 있는 아이템을 옵셔널 체이닝으로 값을 추출해 ViewModel에 Shared인스턴스에 저장
    - 그 데이터로 바텀시트 액션을 실행해 데이터를 넘겨주는 코드 작성
    - 받은 데이터를 화면에 그대로 뿌려주는 SendView구현

--- SharedExtension/SendViewController.swift
+++ SharedExtension/SendViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class SendViewController: UIViewController {
-
+    
+    var vm:ShareViewModel!
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = vm.shared.url?.absoluteString
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false  // AutoLayout 활성화
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white  // 배경색 설정
+        view.addSubview(label)
+        setupConstraints()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-    */
-
 }
+
--- SharedExtension/ShareViewController.swift
+++ SharedExtension/ShareViewController.swift
@@ -8,23 +8,83 @@
 import UIKit
 import Social
 
-class ShareViewController: SLComposeServiceViewController {
-
+final class ShareViewController: SLComposeServiceViewController {
+    
+    var vm = ShareViewModel()
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getSharedData()
+    }
+    func getSharedData() {
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            // 링크를 처리
+            if let itemProviders = item.attachments {
+                itemProviders.forEach { itemProvider in
+                    // URL 타입이 있을 경우
+                    if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                        itemProvider.loadItem(forTypeIdentifier: "public.url") { (url, error) in
+                            if let shareURL = url as? URL {
+                                self.vm.shared.url = shareURL
+                            } else {
+                                print("Error loading URL: \(error?.localizedDescription ?? "")")
+                            }
+                        }
+                    }
+                    
+                    // 이미지 처리
+                    if itemProvider.hasItemConformingToTypeIdentifier("public.image") {
+                        itemProvider.loadItem(forTypeIdentifier: "public.image") { (data, error) in
+                            if let data = data as? Data {
+                                self.vm.shared.image = data
+                            } else {
+                                print("Error loading image: \(error?.localizedDescription ?? "")")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    func presentBottomSheet() {
+        let bottomSheetVC = SendViewController()
+        bottomSheetVC.vm = self.vm
+        if let sheet = bottomSheetVC.sheetPresentationController {
+            sheet.detents = [.medium(), .large()] // Bottom Sheet 크기 조절
+            sheet.prefersGrabberVisible = true    // 위에 핸들바 표시
+        }
+        present(bottomSheetVC, animated: true)
+    }
+    
+    //Post 버튼 활성화/비활성화
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+        
+        return !contentText.isEmpty //텍스트가 비어있는지 여부에 따라 활성화/비활성화
     }
-
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    //Post 버튼이 눌렸을 때
+    override func didSelectPost() {
+        
+        // Share Extension 종료
+        
+        self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
-
+    
+    //compose View에 커스텀으로 아이템을 표시해야할 때
     override func configurationItems() -> [Any]! {
         // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+        let item = SLComposeSheetConfigurationItem()
+        item?.title = "친구에게 공유하기"
+        item?.value = "콰랑"
+        item?.tapHandler = {
+            self.presentBottomSheet()
+//            let viewController = SendViewController()
+//            self.pushConfigurationViewController(viewController)
+        }
+        return [item!]
     }
-
+    
 }
+
+
